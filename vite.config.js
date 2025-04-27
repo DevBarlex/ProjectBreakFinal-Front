26758605d0653f40cec5d6ec5f276aee15f27c60
DevBarlex <alexispaniagua1@gmail.com>
2025-04-27 23:22:11 +0200

    Cambios

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
 export default {
     server: {
-      host: '0.0.0.0',  
+      host: '0.0.0.0', 
       port: process.env.PORT || 5173,  
+      allowedHosts: [
+        'projectbreakfinal-front.onrender.com', 
+      ],
     },
   };
   
